b108981ebb749c1b04235cb30784e820ed32ca37
Nitipat L <iamsvz@gmail.com>
2018-02-21 21:10:16 +0700

    add some string testing

--- books/An Introduction to Programming in Go/03-Types/main.go
+++ books/An Introduction to Programming in Go/03-Types/main.go
@@ -3,6 +3,13 @@ package main
 import "fmt"
 
 func main() {
+
+	// Number
 	fmt.Println("1.0 + 1.5 =", 1.0+1.5)
 	fmt.Println("1 + 1 =", 1+1)
+
+	// String
+	fmt.Println(len("Hello World")) // A space is also considered a character
+	fmt.Println("Hello World"[1])   // represent to a byte
+	fmt.Println("Hello " + "World") // string will be concat not addition
 }

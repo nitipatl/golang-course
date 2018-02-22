37b7a0ceae93123de6c34abaf2a3b27fdbeaae1d
Nitipat L <iamsvz@gmail.com>
2018-02-22 16:16:30 +0700

    add multiple values

--- books/An Introduction to Programming in Go/07-Functions/playground/main.go
+++ books/An Introduction to Programming in Go/07-Functions/playground/main.go
@@ -2,11 +2,22 @@ package main
 
 import "fmt"
 
+// func main() {
+// 	xs := []float64{98, 93, 77, 82, 83}
+// 	fmt.Println(average(xs))
+// }
+
+// func average(numbers []float64) float64 {
+// 	total := 0.0
+// 	for _, v := range numbers {
+// 		total += v
+// 	}
+// 	return total / float64(len(numbers))
+// }
 func main() {
-	xs := []float64{98, 93, 77, 82, 83}
-	total := 0.0
-	for _, v := range xs {
-		total += v
-	}
-	fmt.Println(total / float64(len(xs)))
+	x, y := f2()
+	fmt.Println(x, y)
+}
+func f2() (int, int) {
+	return 5, 6
 }
--- books/An Introduction to Programming in Go/07-Functions/readme.md
+++ books/An Introduction to Programming in Go/07-Functions/readme.md
@@ -1,1 +1,12 @@
-**Range**
+**Return multiple values**  
+
+```go
+func f() (int, int) {
+     return 5, 6
+}
+func main() {
+     x, y := f()
+}
+```
+
+Multiple values are often used to return an error value along with the result (x, err := f()), or a boolean to indicate success (x, ok := f()).

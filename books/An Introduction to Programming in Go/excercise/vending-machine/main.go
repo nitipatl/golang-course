26a82f4ae4417056c4751426a25f8a9379ec310c
Nitipat L <iamsvz@gmail.com>
2018-02-28 01:15:06 +0700

    add machine

--- books/An Introduction to Programming in Go/excercise/vending-machine/main.go
+++ books/An Introduction to Programming in Go/excercise/vending-machine/main.go
@@ -17,27 +17,60 @@ var drickPrices = map[string]int{
 	"DW": 7,  // drinking water
 }
 
-type Wallet struct {
-	total int
+type Machine struct {
+	total          int
+	selectedDrinks string
 }
 
-func (_wallet *Wallet) addCoin(coin string) int {
-	if val, err := coinValues[coin]; err {
-		_wallet.total += val
+func (_machine *Machine) addCoin(coin string) int {
+	if val, ok := coinValues[coin]; ok {
+		_machine.total += val
 	}
-	return _wallet.total
+	return _machine.total
 }
 
-func (_wallet *Wallet) getTotal() int {
-	return _wallet.total
+func (_machine *Machine) getTotal() int {
+	return _machine.total
+}
+
+func (_machine *Machine) selectDrink(action string) string {
+	if val, ok := drickPrices[action]; ok {
+		if val <= _machine.total {
+			_machine.total -= val
+			_machine.selectedDrinks += action + " "
+		}
+	}
+	return _machine.selectedDrinks
+}
+
+func (_machine *Machine) selectReturn() (totalReturn string) {
+	var keys []string
+	for k := range coinValues {
+		keys = append(keys, k)
+	}
+	for _, k := range keys {
+		if coinValues[k] <= _machine.total {
+			coins := _machine.total / coinValues[k]
+			for i := 0; i < coins; i++ {
+				totalReturn += k + " "
+			}
+			_machine.total %= coinValues[k]
+		}
+	}
+	return
 }
 
 func main() {
-	_wallet := Wallet{0}
+	_machine := Machine{total: 0, selectedDrinks: ""}
 
-	_wallet.addCoin("T")
-	_wallet.addCoin("TW")
+	_machine.addCoin("T")
+	_machine.addCoin("TW")
+	_machine.addCoin("O")
 
-	fmt.Println("Currently inserted money:", _wallet.getTotal())
+	fmt.Println("Currently inserted money:", _machine.getTotal())
+
+	fmt.Println(_machine.selectDrink("DW"))
+
+	fmt.Println("Return:", _machine.selectReturn())
 
 }

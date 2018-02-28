621045ae1dac228aa27c0c1949f3eb2fe5704672
Nitipat L <iamsvz@gmail.com>
2018-02-28 14:43:17 +0700

    specify direction of channel arg

--- books/An Introduction to Programming in Go/10-Concurrency/playground/main.go
+++ books/An Introduction to Programming in Go/10-Concurrency/playground/main.go
@@ -5,18 +5,19 @@ import (
 	"time"
 )
 
-func pinger(c chan string) {
+func pinger(c chan<- string) {
 	for i := 0; ; i++ {
 		c <- "ping"
 	}
 }
-func ponger(c chan string) {
+func ponger(c chan<- string) {
 	for i := 0; ; i++ {
 		c <- "pong"
 	}
 }
-func printer(msg <-chan string) {
+func printer(c <-chan string) {
 	for {
+		msg := <-c
 		fmt.Println(msg)
 		time.Sleep(time.Second * 1)
 	}
